578794437eed4b1e57a050a4e6e16a9d923f2506
Vinayaka-Voleti <vinayaka.vvk@gmail.com>
2025-02-21 23:39:56 +0530

    Lab4

---
+++ LAB4/Prog3_2.java
@@ -0,0 +1,17 @@
+//Accept 10 numbers from command line and check how many of them are even and how many are odd.
+public class Prog3_2 {
+    public static void main(String[] args) {
+        int counteven = 0;
+        int countodd = 0;
+        int[] check = {12,13,14,15,16,17,18,19,20,21};
+        for(int i = 0; i<10;i++){
+            if (check[i] % 2 == 0) {
+                counteven++;
+            }
+            else{
+                countodd++;
+            }
+        }
+        System.out.println("No. of Even numbers ="+counteven+"\nNo. of odd numbers ="+countodd);
+    }
+}
