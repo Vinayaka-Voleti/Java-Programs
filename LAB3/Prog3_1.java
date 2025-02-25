47f597405e4c6b7aab22d9ce89edc04d2d8ec10f
Vinayaka-Voleti <vinayaka.vvk@gmail.com>
2025-02-25 22:12:35 +0530

    Lab3

--- LAB3/Prog3_1.java
+++
@@ -1,5 +0,0 @@
-public class Prog3_1 {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ LAB3/box0.java
@@ -0,0 +1,22 @@
+class Box {
+    double length;
+    double width;
+    double height;
+
+    double volume() {
+        return length * width * height;
+    }
+}
+public class box0 {
+    public static void main(String[] args) {
+        Box myBox = new Box();
+
+        myBox.length = 5.0;
+        myBox.width = 4.0;
+        myBox.height = 3.0;
+
+        System.out.println("Volume of the box: " + myBox.volume());
+        // System.out.println("Dimensions of the box " + myBox.dim());
+    }
+}
+
